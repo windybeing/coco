94541cdcd24cb116e2d3f8543c834b2aa3261ad2
Zhiyuan <jason73@sjtu.edu.cn>
2022-09-13 06:52:59 +0000

    revision eval 11 machine

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
-servers="172.31.34.24;172.31.34.242;172.31.41.134;172.31.46.64;172.31.38.97;172.31.36.42;172.31.42.221;172.31.39.159"
-# servers="172.31.34.24;172.31.34.242;172.31.41.134;172.31.46.64"
+servers="172.31.34.24;172.31.34.242;172.31.41.134;172.31.46.64;172.31.38.97;172.31.36.42;172.31.42.221;172.31.39.159;172.31.40.194;172.31.41.167"
+# servers="172.31.34.24;172.31.34.242"
 server_array=$(echo $servers | tr ";" "\n")
 echo $server_array
 
 for server in $server_array; do
     ssh $server "tmux kill-session -t coco"
+done
+pkill bench_tpcc
+
+for server in $server_array; do
     rsync -aqzP --port=22 --force ./tpcc.sh $server:/home/ubuntu/coco/
     rsync -aqzP --port=22 --force ./bench_tpcc $server:/home/ubuntu/coco/
 done
 
 id=1
-# for addr in $server_array; do
-#     ssh $addr "cd coco; tmux new-session -d -s coco \"./tpcc.sh $id\"" &
-#     ((id+=1));
-# done
-# ./tpcc.sh 0
+for addr in $server_array; do
+    ssh $addr "cd coco; tmux new-session -d -s coco \"./tpcc.sh $id\"" &
+    ((id+=1));
+done
+./tpcc.sh 0
